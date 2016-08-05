6dac847cece3de7ee964712450a6d186bf0f60f7
wabarr <wabarr@gmail.com>
2016-08-05 11:53:02 -0400

    rebuild site because I changed one of the includes

--- REBUILD-SITE.R
+++ REBUILD-SITE.R
@@ -1,2 +1,9 @@
 #Be sure to run from the base directory of the project
-lapply(list.files(pattern = "*.Rmd",recursive = TRUE), rmarkdown::render)
+
+tryRender <- function(x){
+  tryCatch(
+  rmarkdown::render(x)
+  , error = function(e) print("Error rendering markdown")
+  )
+}
+lapply(list.files(pattern = "*.Rmd",recursive = TRUE), tryRender)

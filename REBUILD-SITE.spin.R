9b622565f65106e0058b7e1d44291679f248e5b7
wabarr <wabarr@gmail.com>
2016-08-25 16:43:34 -0400

    switch to ioslides and normal rmarkdown for presentation, move and rename automation pres to new folder

--- REBUILD-SITE.spin.R
+++
@@ -1,9 +0,0 @@
-#Be sure to run from the base directory of the project
-
-tryRender <- function(x){
-  tryCatch(
-  rmarkdown::render(x)
-  , error = function(e) print(paste0("Error with ", x))
-  )
-}
-lapply(list.files(pattern = "*.Rmd",recursive = TRUE), tryRender)
